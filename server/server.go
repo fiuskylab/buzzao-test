8bf25cb54f8725dd30c9a4e0ca86f4fa633a64aa
Rafael <rafael_breno@outlook.com>
2021-08-15 02:49:44 -0300

    feat: Add server endpoints

--- server/server.go
+++ server/server.go
@@ -36,6 +36,8 @@ const (
 )
 
 func (s *Server) Listen(port string) {
+	s.App.Post("/config/:n", s.configEndpoint)
+	s.App.Post("/process", s.processEndpoint)
 
 	s.App.Listen(port)
 }
@@ -46,3 +48,60 @@ type NumBody struct {
 
 type j map[string]interface{}
 
+func (s *Server) processEndpoint(c *fiber.Ctx) error {
+	if s.P == nil {
+		return c.
+			Status(http.StatusUnprocessableEntity).
+			JSON(j{
+				"error": "Worker Pool no set yet, set using the endpoint POST: /config/:n ",
+			})
+	}
+	numBody := new(NumBody)
+
+	if err := c.BodyParser(numBody); err != nil {
+		return c.
+			Status(http.StatusUnprocessableEntity).
+			JSON(j{
+				"error": err.Error(),
+			})
+	}
+
+	d := data.NewData(s.P.Concurrency, numBody.Nums)
+
+	s.P.AddJob(workerpool.NewJob(data.EachBatch, d))
+	s.P.Run()
+
+	return c.
+		Status(http.StatusOK).
+		JSON(j{
+			"result": d.Sum,
+		})
+}
+
+func (s *Server) configEndpoint(c *fiber.Ctx) error {
+	n, err := utils.StrToInt(c.Params("n"))
+	if err != nil {
+		return c.
+			Status(http.StatusUnprocessableEntity).
+			JSON(j{
+				"error": err.Error(),
+			})
+	}
+
+	p := workerpool.NewPool(make([]*workerpool.Job, 0), n)
+	s.P = p
+
+	if err := s.P.SetConcurrency(n); err != nil {
+		return c.
+			Status(http.StatusUnprocessableEntity).
+			JSON(j{
+				"error": err.Error(),
+			})
+	}
+
+	return c.
+		Status(http.StatusOK).
+		JSON(j{
+			"message": fmt.Sprintf("Number of threads set to %d", n),
+		})
+}
